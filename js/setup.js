46a923cb5ddb9b20cfbe896b13a950b7b8676d36
Kuvi <anastasia.kuvanova@gmail.com>
2018-02-01 21:39:53 +0300

    intermediate version

--- js/setup.js
+++ js/setup.js
@@ -1,30 +1,63 @@
 'use strict';
 
-var FIRST_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-var SECOND_NAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var DataMap = {
+  FIRST_NAMES: ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'],
+  SECOND_NAMES: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
+  COAT_COLORS: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
+  EYES_COLORS: ['black', 'red', 'blue', 'yellow', 'green'],
+};
+
+var KeyMap = {
+  ESC_CODE: 27,
+  ENTER_CODE: 13
+};
+
+var HIDDEN = 'hidden';
 
 var setupDialog = document.querySelector('.setup');
-setupDialog.classList.remove('hidden');
-
+var setupClose = setupDialog.querySelector('.setup-close');
 var setupSimilar = setupDialog.querySelector('.setup-similar');
+var setupInput = setupDialog.querySelector('.setup-user-name');
+var setupSubmit = setupDialog.querySelector('.setup-submit');
+var setupOpen = document.querySelector('.setup-open');
+var setupOpenIcon = setupOpen.querySelector('.setup-open-icon');
 var wizardTemplate = document.querySelector('#similar-wizard-template').content;
 
 var getRandomNumder = function (min, max) {
   return Math.floor(min + Math.random() * (max - min));
 };
 
+var getRandomItem = function (array) {
+  var index = getRandomNumder(0, array.length);
+  return array[index];
+};
+
 var createWizards = function (count) {
   var resultWizards = [];
   for (var i = 0; i < count; i++) {
     resultWizards.push({
-      name: FIRST_NAMES[getRandomNumder(0, FIRST_NAMES.length)] + ' ' + SECOND_NAMES[getRandomNumder(0, SECOND_NAMES.length)],
-      coatColor: COAT_COLORS[getRandomNumder(0, COAT_COLORS.length)],
-      eyesColor: EYES_COLORS[getRandomNumder(0, EYES_COLORS.length)]
+      name: getRandomItem(DataMap.FIRST_NAMES) + ' ' + getRandomItem(DataMap.SECOND_NAMES),
+      coatColor: getRandomItem(DataMap.COAT_COLORS),
+      eyesColor: getRandomItem(DataMap.EYES_COLORS),
     });
   }
   return resultWizards;
+};
+
+var showSetupDialog = function () {
+  setupDialog.classList.remove(HIDDEN);
+  document.addEventListener('keydown', setupDialogEscKeyDownHandler);
+};
+
+var hideSetupDialog = function () {
+    setupDialog.classList.add(HIDDEN);
+    document.removeEventListener('keydown', setupDialogEscKeyDownHandler);
+};
+
+var setupDialogEscKeyDownHandler = function (evt) {
+  if (evt.keyCode === KeyMap.ESC_CODE) {
+    hideSetupDialog();
+  }
 };
 
 var renderWizard = function (wizard) {
@@ -35,6 +68,43 @@ var renderWizard = function (wizard) {
   return wizardElement;
 };
 
+setupOpen.addEventListener('click', function () {
+  showSetupDialog();
+});
+
+setupClose.addEventListener('click', function () {
+  hideSetupDialog();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === KeyMap.ENTER_CODE) {
+    hideSetupDialog();
+  }
+});
+
+setupOpenIcon.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === KeyMap.ENTER_CODE) {
+    showSetupDialog();
+  }
+});
+
+setupInput.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === KeyMap.ESC_CODE) {
+    evt.stopPropagation();
+  }
+});
+
+setupSubmit.addEventListener('click', function(evt) {
+  hideSetupDialog();
+});
+
+
+setupSubmit.addEventListener('keydown', function(evt) {
+  if (evt.keyCode === KeyMap.ENTER_CODE) {
+    hideSetupDialog();
+  }
+});
+
 var wizards = createWizards(4);
 
 var fragment = document.createDocumentFragment();
@@ -43,4 +113,3 @@ wizards.forEach(function (wizard) {
 });
 
 setupSimilar.querySelector('.setup-similar-list').appendChild(fragment);
-setupSimilar.classList.remove('hidden');
